2d6b42b6faab9f3044cf227dcd1bae0400cc2288
Nikolay Naumchenko <dhnikolas@gmail.com>
2025-05-21 01:11:55 +0300

    Add lock to create

--- internal/services/states/create.go
+++ internal/services/states/create.go
@@ -8,6 +8,10 @@ import (
 
 func (s *StateService) Create(ctx context.Context, opts *dto.ResourceCreateOpts) (*dto.Resource, error) {
 	return s.repo.TxWrap(ctx, func(tx pgx.Tx) (*dto.Resource, error) {
+		err := s.repo.Lock(ctx, tx, &opts.ResourceID)
+		if err != nil {
+			return nil, err
+		}
 		result, err := s.repo.Create(ctx, tx, opts)
 		if err != nil {
 			return nil, err
